96fb24bad3ab0f70330cfe0c2249f89d0e02b212
Baris Terzioglu, Umut <Umut.Baris-Terzioglu@adesso-insurance-solutions.de>
2026-04-12 17:43:44 +0200

    big

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
-const MICROSOFT_CLARITY_PROJECT_ID = 'walpfs3c2q';
+const GOATCOUNTER_ENDPOINT = 'https://almanya101de.goatcounter.com/count';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://almanya101.com'),
@@ -103,13 +103,12 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-          {`(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "${MICROSOFT_CLARITY_PROJECT_ID}");`}
-        </Script>
+        <Script
+          id="goatcounter"
+          src="https://gc.zgo.at/count.js"
+          data-goatcounter={GOATCOUNTER_ENDPOINT}
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
